6c6c9f235dfa4a631b37fe5282d0e7ccbee434c1
Aleksin Danila Sergeevich <69359758+fibonachi09kz@users.noreply.github.com>
2022-09-27 11:08:57 +0600

    add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+    jQuery(function() {
+        let expandBody = $('[data-expand]');
+        expandBody.each(function() {
+            let btn = $(this).find('[data-expand-button]');
+            let content = $(this).find('[data-expand-content]');
+            btn.click(function() {
+                $(this).toggleClass('opened');
+                if ($(this).hasClass('opened')) {
+                    content.removeClass('hidden')
+                } else {
+                    content.addClass('hidden')
+                }
+            })
+        })
+    })
 
 
 
