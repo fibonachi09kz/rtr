72712081f4f9a74f7490fb5cbba68df857ff06c0
Danila Sergeevich <danya09kz@gmail.com>
2022-09-28 02:42:12 +0600

    ..

--- js/main.js
+++ js/main.js
@@ -16,7 +16,58 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 
+    jQuery(function() {
+        let parent = $('.tabs__section');
+        parent.each(function() {
+            let switchers = $(this).find('.tab');
+            let sections = $(this).find('.section');
+            if ( switchers ) {
+                let defaultTarget = $(this).find('.tab.active').data('section');
+                sections.each(function() {
+                    $(this).hide();
+                    if ( $(this).data('section') == defaultTarget ) {
+                        $(this).show();
+                    }
+                });
+                switchers.on('click', function() {
+                    if ( !$(this).hasClass('active') && $(this).find('a').length == 0) {
+                        switchers.removeClass('active');
+                        let targetSection = $(this).data('section');
+                        sections.each(function() {
+                            $(this).hide();
+                            if ( $(this).data('section') == targetSection ) {
+                                $(this).show();
+                            }
+                        });
+                        $(this).addClass('active');
+                    }
+                });
+            }
+        });
+    });
 
+    const reviewsSwiper = new Swiper('.reviews__swiper', {
+        spaceBetween: 30,
+        slidesPerView: 4,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+        }
+    });
+    const brandsSwiper = new Swiper('.brands__swiper', {
+        loop: true,
+        freeMode: true,
+        spaceBetween: 60,
+        grabCursor: true,
+        autoplay: {
+            delay: 1,
+            disableOnInteraction: true
+        },
+        freeMode: true,
+        speed: 5000,
+        freeModeMomentum: false,
+        slidesPerView: 'auto'
+    });
 
 
 
